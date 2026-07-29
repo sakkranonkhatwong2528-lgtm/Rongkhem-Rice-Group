ff4fb7d827a814dad709dc43fcb0627cf8b43b10
sakkranonkhatwong2528-lgtm <sakkranonkhatwong2528@gmail.com>
2026-07-29 21:38:04 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,89 @@
-let members = JSON.parse(localStorage.getItem("members")) || [
+// ฐานข้อมูลสมาชิกกลุ่มข้าวสารบ้านร่องเข็ม
+
+let members = JSON.parse(
+localStorage.getItem("members")
+) || [
 
 {
-    id:1,
-    name:"นายจักร์กวัส ประพลรัตนัง",
-    house:"2",
-    rice:"12"
+id:1,
+name:"นายจักรวัตร ประพลรัตนัง",
+house:"2",
+rice:"12"
 }
 
 ];
 
 
-function saveMembers(){
+// บันทึกข้อมูล
+function saveData(){
 
 localStorage.setItem(
 "members",
 JSON.stringify(members)
 );
+
+}
+
+
+// เพิ่มสมาชิก
+function addData(name,house,rice){
+
+let newMember={
+
+id:Date.now(),
+
+name:name,
+
+house:house,
+
+rice:rice
+
+};
+
+
+members.push(newMember);
+
+saveData();
+
+}
+
+
+// ลบสมาชิก
+function deleteData(id){
+
+members = members.filter(
+m => m.id != id
+);
+
+saveData();
+
+}
+
+
+// แก้ไขสมาชิก
+function editData(id,name,house,rice){
+
+let member = members.find(
+m=>m.id==id
+);
+
+
+if(member){
+
+member.name=name;
+member.house=house;
+member.rice=rice;
+
+saveData();
+
+}
+
+}
+
+
+// ดึงข้อมูลสมาชิก
+function getMembers(){
+
+return members;
 
 }
